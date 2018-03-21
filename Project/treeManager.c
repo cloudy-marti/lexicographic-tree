4b47a21bbcdb546cacf4c92f8fd6965a2e4b10a5
SAJDAK LAURINE <lsajdak@localhost.localdomain>
2018-03-21 17:10:56 +0100

    attention : mettre main de treemanager en commentaire + fonctions de present et affichage à jour

--- Project/treeManager.c
+++ Project/treeManager.c
@@ -4,38 +4,98 @@
 
 /* ici les fonctions qui vont gérer la structure de l'arbre */
 /* !!! PAS DE FONCTION MAIN !!! */
+
+
+Tree allocationNode(char letter){
+	Tree newtree=(Tree)malloc(sizeof(Node));
+	if (newtree != NULL) {
+		newtree->leftChild=NULL;
+		newtree->rightBro=NULL;
+		newtree->letter=letter;
+	}
+	return newtree;
+}
+
+
 // fonction placée à l'arrache, à déplacer le temps voulu
 // search a word in our tree.
-Tree search(Tree tree, char* word){
+
+int search(Tree tree, char* word){
 
 	if(tree == NULL)
-		return NULL;    
+		return 0;    
 	if(tree->letter == *word){
 		if(*word == '\0')
-			return tree;
+			return 1;
 		else 
-			return search(tree->leftChild, word++);
-	}else if(tree->letter < *word){
-		return NULL;
-	}else if(tree->letter > *word){
-		return search(tree->rightBro, word++);
-	}else 
-		return NULL;
-} // est-ce qu'on ne retourne pas plutôt un int ? pour dire oui ou non le mot est là
+			return search(tree->leftChild, word+1);
+	}
+	else if(tree->letter > *word)
+			return 0;
+		else 
+			if(tree->letter < *word)
+				return search(tree->rightBro, word);
+			else 
+				return 0;
+} 
+
+
+void printWord(Tree a, char* buffer, int index)
+{
+	if(a!=NULL)
+	{
+		buffer[index]=a->letter;
+
+		if(a->letter=='\0')
+			printf("%s\n", buffer);
+
+		else 
+			printWord(a->leftChild, buffer, index+1);
+
+		printWord(a->rightBro, buffer, index);
+	}
+}
+
+
+
+
 
 // exemple nul de main en considérant que l'arbre est rempli
-// int main(int argc, char** argv){
-//     Tree a;
-//     char word[20]; // au hasard balathazar
-//     int cmpt=0; //va servir pour aller de case en case dans notre fonction search. 
-//     printf("\nHello. Which word would you like to search in our splendid lexical tree? \n");
-//     scanf("%d", &word);
-    
-//     if((search(a, word, cmpt))!=NULL)
-// 			printf("%d is in our tree. Cool!\n", word);
+int main(int argc, char** argv){
+   Tree tree;
 
-// 		else 
-//             printf("%d isn'tree in our tree. Sad.", word);
+   tree=allocationNode('c');
+   tree->leftChild=allocationNode('e');
+   tree->leftChild->leftChild=allocationNode('\0');
+   tree->leftChild->leftChild->rightBro=allocationNode('s');
+   tree->leftChild->leftChild->rightBro->leftChild=allocationNode('\0');
+   tree->rightBro=allocationNode('d');
+   tree->rightBro->leftChild=allocationNode('e');
+   tree->rightBro->leftChild->leftChild=allocationNode('s');
+   tree->rightBro->leftChild->leftChild->leftChild=allocationNode('\0');
+   tree->rightBro->rightBro=allocationNode('l');
+   tree->rightBro->rightBro->leftChild=allocationNode('e');
+   tree->rightBro->rightBro->leftChild->leftChild=allocationNode('\0');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro=allocationNode('s');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro->leftChild=allocationNode('\0');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro->rightBro=allocationNode('t');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro->rightBro->leftChild=allocationNode('t');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro->rightBro->leftChild->leftChild=allocationNode('r');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro->rightBro->leftChild->leftChild->leftChild=allocationNode('e');
+   tree->rightBro->rightBro->leftChild->leftChild->rightBro->rightBro->leftChild->leftChild->leftChild->leftChild=allocationNode('\0');
+    char word[20]; // au hasard balathazar
+    char buff[20];
+    int i=0;
+    printf("\nHello. Which word would you like to search in our splendid lexical tree? \n");
+    scanf("%s", word);
     
-// 	return EXIT_SUCCESS;
-// }
+   if(search(tree, word))
+		printf("%s present\n", word);
+
+	else
+		printf("%s absent\n", word);
+
+	printWord(tree, buff, i);
+    
+	return EXIT_SUCCESS;
+}
